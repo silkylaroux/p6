1e6b320d2598cfd161e20894fa8cfe73434527b6
silkylaroux <damian.mikayla@gmail.com>
2017-09-23 23:46:03 -0600

    I have failed you Anakin

--- gs.py
+++ gs.py
@@ -1,74 +1,116 @@
-if __name__ == '__main__':
-    import stable_matching_helpers as helpers
-    import ast
-    import json
-    import sys
-    import defaultdict
-
-    with open (sys.arg[1],'r') as f:
-        data = ast.literal_eval(f.read())
-
-    finalList = []
-
-    for x in data:
-        menDict = data[x][0]
-        womenDict = data[x][1]
-        
-        freeMenList = dict.keys(menDict)
-        freeWomenList = dict.keys(womenDict)
-
-        galeDict = {}
-        gale_shapely(freeMenList,freeWomenList,menDict,womenDict,galeDict)
-
+import stable_matching_helpers as helpers
+import ast
+import json
+import sys
 
 
     # freeMen should be the list of men free, which each have a list of free women
-    def gale_shapely(menL,womenL,menD, womenD, dictionary):
-        while has_free(menL) and not has_proposals(menL):
+def gale_shapely():
 
-            #if the woman of the first free man is free
-            if isFree(menD.get(menL[0])[0],womenL):
+    while has_free(freeMenList) and has_proposals(freeMenList, menDict):
 
-                join(menL[0],menD.get(menL[0])[0])
+        # if the woman of the first free man is free
+        # print(menDict.get(freeMenList[0]))
+        if isFree(menDict.get(freeMenList[0])[0], freeWomenList):
+            #print(menDict.get(freeMenList[0])[0])
+            join(freeMenList[0], menDict.get(freeMenList[0])[0])
+
+        else:
+            #print(menDict.get(freeMenList[0])[0])
+            if prefers_Current(menDict.get(freeMenList[0])[0], freeMenList[0]):
+                #print(menDict[freeMenList[0]])
+                menDict[freeMenList[0]].remove(menDict.get(freeMenList[0])[0])
+                #print(menDict[freeMenList[0]])
 
             else:
-                if prefersCurrent(freeMen[0][0],freeMen[0]):
-                    pass
-                else:
-                    endMarriage(freeMen[0][0])
-                    join(freeMen[0][0])
+                end_Marriage(freeMenList[0], menDict.get(freeMenList[0])[0])
+                join(freeMenList[0], menDict.get(freeMenList[0])[0])
+
 
 
     #puts men and woman back in free
-    def end_Marriage(man, woman, menL, womenL):
+def end_Marriage(man, woman):
+    if galeDict.get(man) is not None:
+        galeDict.pop(man)
+    freeMenList.append(man)
+    freeWomenList.append(woman)
 
 
     # combines man and woman, and removes from free lists
-    def join(menL, womenL):
-        pass
+def join(man, woman):
+
+    menDict[man].remove(woman)
+
+    galeDict.update({man: woman})
 
 
-    def prefers_Current(woman, man, womenL):
-        pass
+
+    freeMenList.remove(man)
+    freeWomenList.remove(woman)
 
 
-    #freemen list isn't null
-    def has_free(menL):
-        if menL is None:
-            return False
-        return True
+
+def prefers_Current(woman, man):
+    if not list(menDict.values()).__contains__(woman):
+        return False
+    
+    else:
+        for key, value in galeDict.iteritems():
+            if value == woman:
+                man2 = galeDict[key]
 
 
-    def isFree(woman,womenL):
-        if woman in womenL:
+        if womenDict.get(woman).index(man) < womenDict.get(woman).index(man2):
             return True
         return False
 
 
-    def has_proposals(menL, menDict):
-        for x in menL:
-            if menDict.get(menL[x]) is None:
-                return False
+    #freemen list isn't null
+def has_free(menL):
+    if len(menL) > 0:
         return True
+    return False
+
+
+def isFree(woman,womenL):
+    if woman in womenL:
+        return True
+    return False
+
+
+def has_proposals(menL, menDict):
+    for x in range(len(menL)):
+        if len(menDict.get(menL[x])) > 0:
+            return True
+    return False
+
+
+def write_json(obj, filename):
+    with open(filename, mode='w') as f:
+        json.dump(obj, f) 
+
+if __name__ == '__main__':
+
+    with open(sys.argv[1], 'r') as f:
+        data = ast.literal_eval(f.read())
+
+    finalList = []
+
+    for x in range(len(data)):
+        menDict = data[x][0]
+        womenDict = data[x][1]
+
+        freeMenList = list(dict.keys(menDict))
+        freeWomenList = list(dict.keys(womenDict))
+
+        galeDict = {}
+        gale_shapely()
+
+        finalList.append(galeDict)
+        write_json(finalList,sys.argv[2])
+    
+
+
+
 
 
